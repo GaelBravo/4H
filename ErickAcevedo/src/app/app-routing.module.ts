28f67ee0b11b17069b2f365114ac53a42ba68e40
ErickAcevedoRamon <almn.erickacevedo@cbtis66.edu.mx>
2023-03-31 09:01:30 -0600

    creacion de rutas
    31/03/2023

--- ErickAcevedo/src/app/app-routing.module.ts
+++ ErickAcevedo/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { Ejer1Component } from './ejercicio/ejer1/ejer1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'eje1', component:Ejer1Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
