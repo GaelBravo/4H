d7095776946e51dd3bc4e7efc44925ec15ea2e28
AxelManuelXolioMorales <129126736+AxelManuelXolioMorales@users.noreply.github.com>
2023-03-31 01:06:53 -0500

    Creacion de componente

--- AxelXolio/src/app/app.module.ts
+++ AxelXolio/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EjerciciosComponent } from './ejercicios/ejercicios.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EjerciciosComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
