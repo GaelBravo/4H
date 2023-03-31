62bf838a549c04884a495fda3ee04784f0b844ed
Stephanny-Grasy-Sacaula-R <129252005+Stephanny-Grasy-Sacaula-R@users.noreply.github.com>
2023-03-30 21:42:48 -0600

    Creacion de rutas y navegacion

--- StephannyGrasySacaulaR/src/app/app-routing.module.ts
+++ StephannyGrasySacaulaR/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { EnvironmentInjector, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Eje1Component } from './Ejercicios/eje1/eje1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'eje1',component: Eje1Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
