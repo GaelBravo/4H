d7f990019aef18a6eff9a561159a9cd02bd23545
Stephanny-Grasy-Sacaula-R <129252005+Stephanny-Grasy-Sacaula-R@users.noreply.github.com>
2023-03-30 19:59:07 -0600

    instalacion de Bootstrap

--- isaurazurita/src/app/app.component.spec.ts
+++ isaurazurita/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'axelXolio'`, () => {
+  it(`should have as title 'isaurazurita'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('axelXolio');
+    expect(app.title).toEqual('isaurazurita');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('axelXolio app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('isaurazurita app is running!');
   });
 });
--- isaurazurita/src/app/app.module.ts
+++ isaurazurita/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Ejer1Component } from './ejercicios/ejer1/ejer1.component';
+import { Eje1Component } from './ejercicios/eje1/eje1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Ejer1Component
+    Ejer1Component,
+    Eje1Component
   ],
   imports: [
     BrowserModule,
