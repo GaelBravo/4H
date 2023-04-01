c2cfc273f17ef6add95acb08e706f9a70f874890
Lopez Perez Guillermo Heriberto <ghlpnow@gmail.com>
2023-03-31 22:44:57 -0600

    Update

--- guillermoHeribertoLopezPerez/src/app/app.module.ts
+++ guillermoHeribertoLopezPerez/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Ejercicio1Component } from './ejer1/ejercicio1/ejercicio1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Ejercicio1Component
   ],
   imports: [
     BrowserModule,
