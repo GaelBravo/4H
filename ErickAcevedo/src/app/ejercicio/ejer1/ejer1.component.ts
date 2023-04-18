4a930f36d78d15fbffc38f828d9c598cdbc87f7a
ErickAcevedoRamon <almn.erickacevedo@cbtis66.edu.mx>
2023-04-18 13:08:34 -0600

    aplicación de metodos
    18/04/2023

--- ErickAcevedo/src/app/ejercicio/ejer1/ejer1.component.ts
+++ ErickAcevedo/src/app/ejercicio/ejer1/ejer1.component.ts
@@ -10,4 +10,13 @@ export class Ejer1Component {
   protected title:string = 'Fundacion Acevedo';
   protected email:string = '';
   protected password:string = '';
+
+  validar(){
+    if(this.email=="ErickJoestar@l.com" && this.password=="1718"){
+    alert("welcome to the velvet room :) " + this.title)
+  }
+  else{
+    alert("la verdad escoje y parece que no escogiste bien :(")
+  }
+  }
 }
