227df515d3d3813fee0a89322ac87d5f75fe502e
Adolfo Alexander ALtamirano Quevedo <almn.adolfoaltamirano@cbtis66.edu.mx>
2023-04-18 14:24:49 -0500

    Aplicacion de metodos

--- AdolfoAltamirano/src/app/ejercicios/eje1/eje1.component.ts
+++ AdolfoAltamirano/src/app/ejercicios/eje1/eje1.component.ts
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./eje1.component.css']
 })
 export class Eje1Component {
+
   protected title: string = "Empresa de Adolfo SA de CV";
   protected email: string = "";
   protected password: string = "";
+
+  validar(){
+    if(this.email=="adolfo@email.com" && this.password=="1234"){
+      alert("Bienvenido al sistema de la "+ this.title);
+    }
+    else{
+      alert("El usuario y la contraseña son incorrectos")
+    }
+  }
 }
