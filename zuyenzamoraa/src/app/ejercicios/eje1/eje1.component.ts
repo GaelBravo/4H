10a16066322d18778da593689b8fb04724c2ef26
zuyen1 <almn.zuyenzamora@cbtis66.edu.mx>
2023-04-18 22:37:30 -0600

    aplicacion de metodos

--- zuyenzamoraa/src/app/ejercicios/eje1/eje1.component.ts
+++ zuyenzamoraa/src/app/ejercicios/eje1/eje1.component.ts
@@ -9,5 +9,13 @@ export class Eje1Component {
 protected title: string= "Empresa de Zuyen a SA de cv"
 protected email: string = '';
 protected password: string = '';
+validar(){
+  if(this.email=="zuyen.com" && this.password=="1234"){
+    alert("bienvenido al sistema " + this.title);
+  }
+  else{
+    alert("El usuario o la contraseña es incorrecta :C");
+  }
+}
 }
 
