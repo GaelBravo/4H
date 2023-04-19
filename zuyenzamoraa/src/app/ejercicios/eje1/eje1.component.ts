0287e072db85d289afe0d68efa290149d10828dc
zuyen1 <almn.zuyenzamora@cbtis66.edu.mx>
2023-04-18 22:33:37 -0600

    directiva ngmodel

--- zuyenzamoraa/src/app/ejercicios/eje1/eje1.component.ts
+++ zuyenzamoraa/src/app/ejercicios/eje1/eje1.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class Eje1Component { 
 protected title: string= "Empresa de Zuyen a SA de cv"
+protected email: string = '';
+protected password: string = '';
 }
+
