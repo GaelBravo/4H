94fe1cf23ef01d0bfdce340d0f6aa13e642e860d
AxelManuelXolioMorales <129126736+AxelManuelXolioMorales@users.noreply.github.com>
2023-04-18 23:12:20 -0500

    directiva ngmodel

--- AxelXolio/src/app/ejercicios/eje1/eje1.component.ts
+++ AxelXolio/src/app/ejercicios/eje1/eje1.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./eje1.component.css']
 })
 export class Eje1Component {
+protected title: string = "Empresa de Xolio SA de CV";
+protected email: string = "";
+protected password: string = "";
 
 }
