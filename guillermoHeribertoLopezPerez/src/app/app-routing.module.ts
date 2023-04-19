bf6ab0223ba9f4724bd5b2f85b76d90277318ab3
Lopez Perez Guillermo Heriberto <ghlpnow@gmail.com>
2023-04-18 18:56:54 -0600

    Creación de rutas y Navegación

--- guillermoHeribertoLopezPerez/src/app/app-routing.module.ts
+++ guillermoHeribertoLopezPerez/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Ejercicio1Component } from './ejer1/ejercicio1/ejercicio1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ejercicio1', component: Ejercicio1Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
