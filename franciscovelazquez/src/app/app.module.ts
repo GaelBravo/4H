58d67aabfd22a3204c23da99dc1c9ae7241516bb
FRANCISCO VELAZQUEZ PIOQUINTO <fv755900@gmail.com>
2023-04-18 22:55:00 -0500

    correccion de todo mi proyecto

--- franciscovelazquez/src/app/app.module.ts
+++ franciscovelazquez/src/app/app.module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Ejer1Component } from './ejercicio/ejer1/ejer1.component';
-import { Proyec1Component } from './proyecto/proyec1/proyec1.component';
-import { Eje1Component } from './ejercicio/eje1/eje1.component';
-import { Pro1Component } from './proyect/pro1/pro1.component';
+import { Eje1Component } from './ejercicios/eje1/eje1.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    Ejer1Component,
-    Proyec1Component,
-    Eje1Component,
-    Pro1Component
+    Eje1Component
   ],
   imports: [
     BrowserModule,
