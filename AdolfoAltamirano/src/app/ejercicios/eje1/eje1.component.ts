d1b01d004e9dd4734ed5e27b8ca1a02dd2e4660f
Adolfo Alexander Altamirano Quevedo <almn.adolfoaltamirano@cbtis66.edu.mx>
2023-04-19 22:34:43 -0500

    creacion de un vector para la verificacion del usuario

--- AdolfoAltamirano/src/app/ejercicios/eje1/eje1.component.ts
+++ AdolfoAltamirano/src/app/ejercicios/eje1/eje1.component.ts
@@ -10,13 +10,19 @@ export class Eje1Component {
   protected title: string = "Empresa de Adolfo SA de CV";
   protected email: string = "";
   protected password: string = "";
+  protected usuario =[
+    {usr:"ernesto@l.com", psw:"1234",nombre:"Ernesto Vergara Ortiz"},
+    {usr:"adolfo@l.com", psw:"12345",nombre:"Adolfo Alexander Altamirano Quevedo"},
+    {usr:"kevin@l.com", psw:"123456",nombre:"Kevin Uriel Amador Cancio"}
+  ];
 
   validar(){
-    if(this.email=="adolfo@email.com" && this.password=="1234"){
-      alert("Bienvenido al sistema de la "+ this.title);
+    for(let i = 0; i < this.usuario.length; i++){
+      if(this.email==this.usuario[i].usr && this.password==this.usuario[i].psw){
+        alert(this.usuario[i].nombre+" Bienvenido al sistema de " + this.title);
+        return;
+      }
     }
-    else{
-      alert("El usuario y la contraseña son incorrectos")
-    }
+    alert("Usuario y contraseña incorrectos");
   }
 }
