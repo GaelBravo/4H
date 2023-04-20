c6ee816a8c833773b633f40447267ddb2570e298
ErickAcevedoRamon <almn.erickacevedo@cbtis66.edu.mx>
2023-04-19 20:43:00 -0600

    vector de usuarios
    19/04/2023

--- ErickAcevedo/src/app/ejercicio/ejer1/ejer1.component.ts
+++ ErickAcevedo/src/app/ejercicio/ejer1/ejer1.component.ts
@@ -10,13 +10,19 @@ export class Ejer1Component {
   protected title:string = 'Fundacion Acevedo';
   protected email:string = '';
   protected password:string = '';
+  protected usuario= [
+                      {usr:"ErickJoestar@l.com", psw:"1718",nombre:"Erick Acevedo Ramón"},
+                      {usr:"DaniHO@l.com", psw:"1234",nombre:"Daniel Omar"},
+                      {usr:"Tilin@l.com", psw:"4321",nombre:"Tilin"},
+                    ]
 
   validar(){
-    if(this.email=="ErickJoestar@l.com" && this.password=="1718"){
-    alert("welcome to the velvet room :) " + this.title)
+    for(let i=0;i<this.usuario.length;i++){
+    if(this.email==this.usuario[i].usr && this.password==this.usuario[i].psw){
+    alert(this.usuario[i].nombre+ "welcome to the velvet room :) " + this.title);
+    return;
   }
-  else{
+  }
     alert("la verdad escoje y parece que no escogiste bien :(")
   }
   }
-}
