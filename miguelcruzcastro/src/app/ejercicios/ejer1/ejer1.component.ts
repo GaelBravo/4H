a7c3c7eaf48ef88dd6e921b6be91d7f8b79b2b3a
MiguelCruzCastro <127871609+MiguelCruzCastro@users.noreply.github.com>
2023-04-20 21:44:03 -0600

    Validacion de datos y creacion de un segundo componente

--- miguelcruzcastro/src/app/ejercicios/ejer1/ejer1.component.ts
+++ miguelcruzcastro/src/app/ejercicios/ejer1/ejer1.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Route, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-ejer1',
@@ -10,12 +11,21 @@ export class Ejer1Component {
   protected title: string = 'Empresa de Miguel SA de CV' ;
   protected email: string = '';
   protected password: string = '';
+  protected usuarios = [
+                                    {usr: "Miguel@l.com",psw:"12345", nombre: "Miguel de Jesus"},
+                                    {usr:"Rubi@l.com",psw:"12345", nombre: "Rubi Ruiz"},
+                                    {usr:"Murcia@l.com",psw:"12345", nombre: "Murcia"}
+  
+                                  ];
+  constructor(private rutas:Router){}
   validar(){
-    if(this.email=="Miguel.com" && this.password=="1234"){
-      alert("bienvenido al sistema " + this.title);
+    for(let i = 0; i <this.usuarios.length; i++){
+      if(this.email==this.usuarios[i].usr && this.password==this.usuarios[i].psw){
+        alert(this.usuarios[i].nombre+" Bienvenido al sistema de la"+ this.title);
+        this.rutas.navigate(["/Home"]);
+        return;
+      }
     }
-    else{
       alert("El usuario o la contraseña es incorrecta :C");
-    }
   }
 }
