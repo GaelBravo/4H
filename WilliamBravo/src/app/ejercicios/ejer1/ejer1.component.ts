c2b496367242fb3226fbd81c071da99617124f83
williambravohudson <127994552+williambravohudson@users.noreply.github.com>
2023-04-20 23:29:27 -0500

    Creacion de un vector

--- WilliamBravo/src/app/ejercicios/ejer1/ejer1.component.ts
+++ WilliamBravo/src/app/ejercicios/ejer1/ejer1.component.ts
@@ -10,14 +10,18 @@ export class Ejer1Component {
 protected title:string = 'Empresa de William SA de CV';
 protected email:string = '';
 protected password:string = '';
+protected usuarios= [{usr:"ernesto@l.com",psw:"1234",nombre:"Ernesto"},
+                     {usr:"rubi@l.com",psw:"12345",nombre:"Rubi"},
+                     {usr:"murcia@l.com",psw:"123456",nombre:"Monica Murcia"}
+                    ];
 
 validar(){
-  if(this.email=="William@l.com" && this.password=="2012"){
-    alert("Bienvenido al sistema"+ this.title);
-  }
-  else{
-    alert("El usuario y/o contraseña no son correctos");
+  for(let i=0; i<this.usuarios.length; i++){
+  if(this.email==this.usuarios[i].usr && this.password==this.usuarios[i].psw){
+    alert(this.usuarios[i].nombre+"Bienvenid@ al sistema"+ this.title);
+    return;
   }
 }
-
+  alert("El usuario y/o contraseña no son correctos"); 
+} 
 }
