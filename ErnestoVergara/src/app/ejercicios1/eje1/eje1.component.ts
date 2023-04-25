ae15f7e7e2c1d7cba78ec79ffebef13dd6c93401
MSC-JorgeCortes <127913049+Ernesto-Vergara-ortiz@users.noreply.github.com>
2023-04-24 21:51:46 -0600

    arrays

--- ErnestoVergara/src/app/ejercicios1/eje1/eje1.component.ts
+++ ErnestoVergara/src/app/ejercicios1/eje1/eje1.component.ts
@@ -10,14 +10,19 @@ export class Eje1Component {
   protected title: string ="empresa de Ernesto SA de CV";
   protected email:string ="";
   protected password:string =""; 
+  protected usuario =[
+                       {usr:"ernesto@l.com", psw:"1234",nombre:"Ernesto Vergara Ortiz"},
+                       {usr:"maria@l.com", psw:"12345",nombre:"Maria Murillo Alvarafo"},
+                       {usr:"kevin@l.com", psw:"123456",nombre:"Kevin Martinez Venancio"}
+                     ];
 
   validar(){
-    if(this.email=="Ernesto@1.com"&&this.password=="1234"){
-      alert("Bienvenidos a este gran sistema "+ this.title);
-    }
-    else{
-      alert("El ususario o la contraseña no son correctos");
+   for(let i = 0; i < this.usuario.length; i++){
+    if(this.email==this.usuario[i].usr && this.password==this.usuario[i].psw){
+      alert(this.usuario[i].nombre+" Bienvenido al sistema de " + this.title);
+      return;
     }
   }
-
+  alert("Usuario y contraseña incorrectos");
+}
 }
