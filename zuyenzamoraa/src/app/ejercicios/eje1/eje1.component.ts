3c737c9f6687f7a1aa32202bced5b6bb1a20521a
zuyen1 <almn.zuyenzamora@cbtis66.edu.mx>
2023-04-24 22:49:00 -0600

    creacion de vector para la verificacion de correo

--- zuyenzamoraa/src/app/ejercicios/eje1/eje1.component.ts
+++ zuyenzamoraa/src/app/ejercicios/eje1/eje1.component.ts
@@ -9,14 +9,20 @@ export class Eje1Component {
 protected title: string= "Empresa de Zuyen a SA de cv"
 protected email: string = '';
 protected password: string = '';
-validar(){
-  if(this.email=="zuyen@gmail.com" && this.password=="1"){
-    alert("bienvenido al sistema " + this.title);
-  }
-  else {
-    alert("Incorrecto ");
-  }
+protected usuario =[
+  {usr:"ernesto@l.com", psw:"1234",nombre:"Ernesto Vergara Ortiz"},
+  {usr:"adolfo@l.com", psw:"12345",nombre:"Adolfo Alexander Altamirano Quevedo"},
+  {usr:"kevin@l.com", psw:"123456",nombre:"Kevin Uriel Amador Cancio"}
+];
 
+validar(){
+  for(let i = 0; i < this.usuario.length; i++){
+    if(this.email==this.usuario[i].usr && this.password==this.usuario[i].psw){
+      alert(this.usuario[i].nombre+" Bienvenido al sistema de " + this.title);
+      return;
+    }
+  }
+  alert("Usuario y contraseña incorrectos");
 }
 }
 
