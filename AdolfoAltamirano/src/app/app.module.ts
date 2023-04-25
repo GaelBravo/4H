ddd6bb16ce5e6ad6075b6a405378ca6076cadb98
Adolfo Alexander Altamirano Quevedo <almn.adolfoaltamirano@cbtis66.edu.mx>
2023-04-25 00:40:16 -0500

    Array (error)

--- AdolfoAltamirano/src/app/app.module.ts
+++ AdolfoAltamirano/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Eje1Component } from './ejercicios/eje1/eje1.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Eje2Component } from './ejercicios/eje2/eje2.component';
 import { Eje3Component } from './ejercicios/eje3/eje3.component';
 
@@ -18,7 +18,9 @@ import { Eje3Component } from './ejercicios/eje3/eje3.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    NgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
