e99f0e70795dd6b841dea60bff131872cadda59e
Rubí Ruíz <almn.rubiruiz@cbtis66.edu.mx>
2023-04-24 22:36:38 -0600

    creacion del component 2 y rutas

--- RubiRuiz/src/app/ejercicios/eje1/eje1.component.ts
+++ RubiRuiz/src/app/ejercicios/eje1/eje1.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-eje1',
@@ -6,18 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./eje1.component.css']
 })
 export class Eje1Component {
+protected title: string = "Empresa de Rubi SA de CV";
+protected email: string = "";
+protected password: string = "";
+protected usuario =[
+  {usr:"ernesto@l.com", psw:"1234",nombre:"Ernesto Vergara Ortiz"},
+  {usr:"adolfo@l.com", psw:"12345",nombre:"Adolfo Alexander Altamirano Quevedo"},
+  {usr:"kevin@l.com", psw:"123456",nombre:"Kevin Uriel Amador Cancio"}
+];
+constructor(private rutas:Router){
 
-  protected title: string = 'Empresa de Rubí SA de CV';
-  protected email: string = '';
-  protected password: string = '';
-  protected usuario =[
-    {usr:"ernesto@l.com", psw:"1234",nombre:"Ernesto Vergara Ortiz"},
-    {usr:"adolfo@l.com", psw:"12345",nombre:"Adolfo Alexander Altamirano Quevedo"},
-    {usr:"kevin@l.com", psw:"123456",nombre:"Kevin Uriel Amador Cancio"}
-  ];
-  rutas: any;
-
- validar(){
+}
+validar(){
   for(let i = 0; i < this.usuario.length; i++){
     if(this.email==this.usuario[i].usr && this.password==this.usuario[i].psw){
       alert(this.usuario[i].nombre+" Bienvenido al sistema de " + this.title);
