3759a6839ca5df0c9a160d1af410618f2a08995d
UrielAmadorCancio <urielkevin628@gmail.com>
2023-05-04 10:06:28 -0500

    validacion del formulario

--- UrielAmador/src/app/ejercicios/eje3/eje3.component.ts
+++ UrielAmador/src/app/ejercicios/eje3/eje3.component.ts
@@ -8,15 +8,32 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class Eje3Component {
   protected title: string = "Empresa de Kevin Uriel SA de CV";
-  passwordForm: FormGroup;
+  protected formulario: FormGroup;
   constructor(private formbuilder:FormBuilder){
-    this.passwordForm = formbuilder.group({
-      password:['',
+    this.formulario = formbuilder.group({
+      password:['',[
       Validators.required,
       Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
       Validators.minLength(8)
-    ]
-    });
+    ]],
+    confirmapassword:['',[
+      Validators.required
+    ]],
+    correo:['',[
+      Validators.required,
+      Validators.email
+    ]],
+    nombre:['',[
+      Validators.required
+    ]]
+
+    },{Validators: this.passwordsIguales});
+  }
+  protected passwordsIguales(formGroup: FormGroup){
+    const pass= formGroup.get('password')?.value || '';
+    const confirmaPass = formGroup.get('confirmaPassword')?.value || '';
+    return pass === confirmaPass? null : {noSonIguales: true};
+
   }
 
 }
