518cbad694a40d110779f084979e0845b68ae2f9
Stephanny-Grasy-Sacaula-R <129252005+Stephanny-Grasy-Sacaula-R@users.noreply.github.com>
2023-05-10 22:49:32 -0600

    Validacion del formulario part1

--- StephannyGrasySacaulaR/src/app/Ejercicios/eje3/eje3.component.ts
+++ StephannyGrasySacaulaR/src/app/Ejercicios/eje3/eje3.component.ts
@@ -8,16 +8,51 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class Eje3Component {
   protected title: string = "Empresa de Stephanny Grasy 7N";
-  passwordForm: FormGroup;
+  protected formulario: FormGroup;
+
   constructor(private formbuilder: FormBuilder){
-    this.passwordForm = this.formbuilder.group({
+    this.formulario = this.formbuilder.group({
       password:['',[
         Validators.required,
         Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
         Validators.maxLength(8)
-      ]]
-    });
+      ]],
+      email:['',[
+        Validators.required,
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]=$'),
+        Validators.minLength(8)
+      ]],
+      
+      confirmaPassword: ['', [
+        Validators.required,
+      ]],
+      correo: ['', [
+        Validators.required,
+        Validators.email,
+      ]],
+      nombre: ['', [
+        Validators.required,
+      ]],
+
+
+    },
+
+    {Validators: this.passwordsIguales}
+
+    );
+
+  
+
   }
+
+  protected passwordsIguales(formGroup: FormGroup){
+    const pass= formGroup.get('password')?.value || '';
+    const confirmaPass = formGroup.get('confirmaPassword')?.value || '';
+    return pass === confirmaPass? null : {noSonIguales: true};
+  }
+
+
+
 
 
 }
