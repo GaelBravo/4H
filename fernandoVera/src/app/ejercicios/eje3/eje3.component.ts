b6afec9014257a302bf260f3e7ba80fb60043d4d
FernandoAngelVer <127986464+FernandoAngelVer@users.noreply.github.com>
2023-05-23 22:37:11 -0600

    eje3

--- fernandoVera/src/app/ejercicios/eje3/eje3.component.ts
+++ fernandoVera/src/app/ejercicios/eje3/eje3.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-eje3',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./eje3.component.css']
 })
 export class Eje3Component {
-
+  protected title: string = "Empresa de Angel SA de CV";
+  passwordForm: FormGroup;
+  constructor(private formbuilder: FormBuilder){
+    password: ['',[
+      Validators.required,
+      Validators.pattern('^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]=$'),
+    ]
+  ];
+  }
 }
