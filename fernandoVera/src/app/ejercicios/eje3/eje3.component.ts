14f20c7a32b138f7d1b6c916e2e9c725fdb4931d
FernandoAngelVer <127986464+FernandoAngelVer@users.noreply.github.com>
2023-05-25 22:21:54 -0600

    Eje3 finalizado

--- fernandoVera/src/app/ejercicios/eje3/eje3.component.ts
+++ fernandoVera/src/app/ejercicios/eje3/eje3.component.ts
@@ -6,6 +6,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './eje3.component.html',
   styleUrls: ['./eje3.component.css']
 })
+
 export class Eje3Component {
   protected title: string = "Empresa de Angel SA de CV";
   passwordForm: FormGroup;
@@ -13,9 +14,27 @@ export class Eje3Component {
     this.passwordForm = this.formbuilder.group({
       password: ['',[
         Validators.required,
-        Validators.pattern('^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]=$'),
         Validators.minLength(8),
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
+      ]],
+      confirmaPassword: ['', [
+        Validators.required,
+      ]],
+      correo: ['', [
+        Validators.required,
+        Validators.email,
+      ]],
+      nombre: ['', [
+        Validators.required,
       ]]
-    });
+    },
+    { Validators: this.passwordIguales });
+  }
+
+  protected passwordIguales(formGroup: FormGroup){
+    const pass = formGroup.get('password')?.value || '';
+    const confirmaPassword = formGroup.get('confirmaPassword')?.value || '';
+
+    return pass === confirmaPassword ? null : { noSonIguales: true };
   }
 }
