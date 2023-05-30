643e3ef1a59cf6ab13a0fc84ab2dc4c2c41e8bce
Rubí Ruíz <almn.rubiruiz@cbtis66.edu.mx>
2023-05-29 20:54:35 -0600

    creacion del eje3 y su rooteo

--- RubiRuiz/src/app/app.module.ts
+++ RubiRuiz/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { Eje1Component } from './ejercicios/eje1/eje1.component';
 import { FormsModule } from '@angular/forms';
 import { Eje2Component } from './ejercicios/eje2/eje2.component';
+import { Eje3Component } from './ejercicios/eje3/eje3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Eje1Component,
-    Eje2Component
+    Eje2Component,
+    Eje3Component
   ],
   imports: [
     BrowserModule,
---
+++ RubiRuiz/src/app/ejercicios/eje3/eje3.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eje3',
+  templateUrl: './eje3.component.html',
+  styleUrls: ['./eje3.component.css']
+})
+export class Eje3Component {
+
+}
